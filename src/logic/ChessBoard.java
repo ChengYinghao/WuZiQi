c3354472f54fa42b4a98e5812ca7539087698112
陈英豪 <1131637423@qq.com>
2017-09-13 22:01:22 +0700

    将ChessBoard类实现了Abstract的接口

--- src/logic/ChessBoard.java
+++ src/logic/ChessBoard.java
@@ -1,8 +1,10 @@
 package logic;
 
+import model.AbstractChessBoard;
 import model.ChessType;
+import org.jetbrains.annotations.NotNull;
 
-public class ChessBoard {
+public class ChessBoard implements AbstractChessBoard {
     protected static final int ROW_COUNT = 15;
     protected static final int COLUMN_COUNT = 15;
     boolean isWhiteTurn = false;
@@ -10,4 +12,19 @@ public class ChessBoard {
     //棋盘
     ChessType[][] board = new ChessType[COLUMN_COUNT][ROW_COUNT];
 
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @NotNull
+    @Override
+    public ChessType get(int row, int column) {
+        return null;
+    }
 }
