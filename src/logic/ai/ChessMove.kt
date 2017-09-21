c854fcec3ca156feca42d3292d9258a5697ca3eb
陈英豪 <1131637423@qq.com>
2017-09-21 16:02:20 +0700

    终于写完了搜索函数

--- src/logic/ai/ChessMove.kt
+++ src/logic/ai/ChessMove.kt
@@ -2,7 +2,9 @@ package logic.ai
 
 data class ChessMove(
 	/**考虑棋子位置不考虑局势的分数*/
-	val score: Int,
-	val layer: Int,
-	val isWhiteTurn: Boolean
+	var pos:ChessPos,
+	var score: Int,
+	var layer: Int,
+	var isWhiteTurn: Boolean
+
 )
---
+++ src/logic/ai/searchEngiine.java
@@ -0,0 +1,100 @@
+package logic.ai;
+
+import logic.chessboard.CYHChessboard;
+import logic.chessboard.ChessType;
+
+import java.util.ArrayList;
+
+public class searchEngiine implements BaseSearchEngine {
+    int maxSearchDepth = 10;
+    CYHChessboard cyhChessboard = new CYHChessboard();
+    BaseEvaluator evaluator;
+    BaseMoveGenerator baseMoveGenerator;
+    ChessMove bestMove;
+
+    @Override
+    public ChessMove searchABestMove(ChessType[][] board, boolean isWhiteTurn) {
+        bestMove = null;
+        NegaSearch(-20000, 20000, maxSearchDepth);
+        return bestMove;
+    }
+
+    @Override
+    public void setSearch(int depth) {
+        maxSearchDepth = depth;
+    }
+
+    @Override
+    public void setEvaluator(BaseEvaluator evaluator) {
+        this.evaluator = evaluator;
+    }
+
+    @Override
+    public void setMoveGenerator(BaseMoveGenerator moveGenerator) {
+        this.baseMoveGenerator = moveGenerator;
+    }
+
+
+    @Override
+    public int isGameOver(ChessType[][] board, int depth) {
+        boolean isWhiteTurn;
+        int score;
+        int i;
+        i = (maxSearchDepth - depth) % 2;
+        isWhiteTurn = i != 0;
+        score = evaluator.evaluate(cyhChessboard.board, isWhiteTurn);
+        if (Math.abs(score) > 8000) {
+            return score;
+        }
+        return 0;
+    }
+
+    @Override
+    public void makeMove(ChessMove chessMove, ChessType type) {
+        cyhChessboard.board[chessMove.getPos().getRow()][chessMove.getPos().getColumn()] = type;
+    }
+
+    @Override
+    public void unMakeMove(ChessMove chessMove) {
+        cyhChessboard.board[chessMove.getPos().getRow()][chessMove.getPos().getColumn()] = ChessType.NONE;
+    }
+
+    @Override
+    public int NegaSearch(int alpha, int beta, int depth) {
+        int side;
+        ChessType type;
+        boolean isWhiteTurn;
+        side = (maxSearchDepth - depth) % 2;
+        if (side == 0) {
+            isWhiteTurn = false;
+            type = ChessType.BLACK;
+        } else {
+            isWhiteTurn = true;
+            type = ChessType.WHITE;
+        }
+        int isGameOver = isGameOver(cyhChessboard.board, depth);
+        if (isGameOver != 0) {
+            bestMove.setScore(isGameOver);
+            return isGameOver;
+        }
+        if (depth <= 0) {
+            int score = evaluator.evaluate(cyhChessboard.board, isWhiteTurn);
+            bestMove = null;
+            return score;
+        }
+        ArrayList<ChessMove> moveList = baseMoveGenerator.createPossibleMove(cyhChessboard.board, depth, isWhiteTurn);
+        for (int i = 0; i < moveList.size(); i++) {
+            makeMove(moveList.get(i), type);
+            int score = -NegaSearch(-alpha, -beta, depth - 1);
+            unMakeMove(moveList.get(i));
+            if (score > alpha) {
+                alpha = score;
+                bestMove = moveList.get(i);
+            }
+            if (alpha >= beta) {
+                break;
+            }
+        }
+        return alpha;
+    }
+}
